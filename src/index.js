12c279867109cc07daf9a612c4016a40be3095a6
aevans <aevans@starbucks.com>
2022-12-10 17:11:09 -0800

    removed unliked create-react-app stuff

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import "./pattern-lib/styles/index.css";
+import "./pattern-lib/styles/font.css";
+import "./pattern-lib/styles/color.css";
+import "./pattern-lib/styles/flex.css";
+import "./pattern-lib/styles/misc.css";
+import "./pattern-lib/styles/padding-margin.css";
+import HomePage from './components/index';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>
 );
 
